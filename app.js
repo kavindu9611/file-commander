f2c61f74f0e24a9f9c1d43394424c3867f668944
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-09-12 13:21:50 +0530

    Add comments

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ const fs = require("fs/promises");
 
   for await (const event of watcher) {
     if (event.eventType === "change") {
-      //the file was changed...
-      // console.log(event);
-
+    
+      //Get the size of the file
       const size = (await commandFileHandler.stat()).size;
+      //Allocate buffer with the size of the file 
       const buff = Buffer.alloc(size);
 
+      //The location at which want to start filling the buffer
       const offset = 0;
+      //How many bytes we want to read
       const length = buff.byteLength;
+      //The position that we want to start reading the file from
       const position = 0;
 
       const content = await commandFileHandler.read(
