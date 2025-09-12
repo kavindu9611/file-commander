97c6d62f449d0a902b6a19ae665b8c13367fba38
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-09-12 09:48:35 +0530

    Reading the content from the command file

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const fs = require("fs/promises");
 
 (async () => {
+  const commandFileHandler = await fs.open("./command.txt", "r");
   //watcher - async iterator
   const watcher = fs.watch("./command.txt");
 
   for await (const event of watcher) {
     if (event.eventType === "change") {
       //the file was changed...
-      console.log(event);
+      // console.log(event);
+
+      const size = (await commandFileHandler.stat()).size;
+      const buff = Buffer.alloc(size);
+
+      const offset = 0;
+      const length = buff.byteLength;
+      const position = 0;
+
+      const content = await commandFileHandler.read(
+        buff,
+        offset,
+        length,
+        position
+      );
+      console.log(content);
     }
   }
 })();
